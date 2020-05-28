3bd10a332eb24d385b7ffb65c018f9d4605b04cb
Krishan Bhadana <krishanbhadana@Krishans-MBP.bbrouter>
2020-05-28 14:36:29 +0530

    added comments to first program

--- 1a) bfs.py
+++ 1a) bfs.py
@@ -1,3 +1,4 @@
+# Initialising a graph
 graph = {
   'A' : ['B','C'],
   'B' : ['D', 'E'],
@@ -7,22 +8,23 @@ graph = {
   'F' : []
 }
 
+#creating 2 empty array for keeping track of visited nodes of graph and the queue for the sequence of access
 visited = []
 queue = [] 
 
 def bfs(visited, graph, node, elementToSearch):
-  visited.append(node)
-  queue.append(node)
-  flag=0
+  visited.append(node)   #when a node is visited , it is added to visited array
+  queue.append(node)    #node added to queue
+  flag=0		   #setting up flag variable to tell when the element is found
 
   while queue:
-    s = queue.pop(0) 
+    s = queue.pop(0)     #getting first element of queue
     print (s, end = " ")
     if s==elementToSearch:
         print ('element found')
         flag=1
         break
-    else:
+    else:			#if neighbours of the element not visited then it is added to the queue			
         for neighbour in graph[s]:
             if neighbour not in visited:
                 visited.append(neighbour)
