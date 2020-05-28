be3658e27605a87b565f5938465f5987e26c39b8
Krishan Bhadana <krishanbhadana@Krishans-MBP.bbrouter>
2020-05-28 14:38:31 +0530

    added comments to first b programm

--- 1b dfs.py
+++ 1b dfs.py
@@ -8,8 +8,11 @@ graph = {
     'F' : []
 }
 
-visited = []
-flag=0
+#creating 2 empty array for keeping track of visited nodes of graph and the queue for the sequence of access
+
+visited = []   #when a node is visited , it is added to visited array
+flag=0	         #setting up flag variable to tell when the element is found
+
 
 def dfs(visited, graph, node,elementToSearch):
     
@@ -20,7 +23,7 @@ def dfs(visited, graph, node,elementToSearch):
             h= input ("Enter 1 to exit")
             sys.exit()
             
-        else:
+        else:		#if neighbours of the element not visited then it is added to the stack	
              visited.append(node)
              for neighbour in graph[node]:
                  dfs(visited, graph, neighbour,elementToSearch)
@@ -29,4 +32,3 @@ def dfs(visited, graph, node,elementToSearch):
 elementToSearch= input ("Enter the element to search: ")
 dfs(visited, graph, 'A',elementToSearch)
 print ('element not found')
-
